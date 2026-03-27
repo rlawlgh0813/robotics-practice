3a7ed18837dd55cd0e892f13ea18618537bd5b23
rlawlgh0813 <jihooo7@naver.com>
2026-03-27 16:30:01 +0900

    3/27 sub 1

---
+++ class/class09/gazebo_ws/build/simple_arm_moveit/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/ssu/robotics-practice/class/class09/gazebo_ws/install/simple_arm_description/share/simple_arm_description/package.sh"
---
+++ class/class09/gazebo_ws/src/simple_arm_trajectory/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'simple_arm_trajectory'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='deeptree',
+    maintainer_email='deeptree@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        'controller_state_monitor = simple_arm_trajectory.controller_state_monitor:main',
+        'send_waypoint = simple_arm_trajectory.send_waypoint:main',
+        'waypoint_action_follower = simple_arm_trajectory.waypoint_action_follower:main',
+        ],
+    },
+)
